2cfd88698d53ef4cb5be5df8fc397bc4efd95ba2
Sean Bradly <seanb@atredis.com>
2022-04-24 12:51:31 -0500

    updates to win10

--- win10.sh
+++ win10.sh
@@ -12,18 +12,25 @@ include platforms
 include disk
 include network
 
+# include local custom libs
+include site/lvmraid-hydra
+include site/vlanbridge
+
 # configure platform 
-platform   win10 # same as "native" + some hyperv support
-memory     8G
-processors 4
+platform   windows10 # same as "native" + some hyperv support
+memory     32G
+processors 16,sockets=1,cores=8,threads=2
+
+QEMU_MEM="-m 32G -mem-path /dev/hugepages"
+# FOREGROUND=true # use qemu gui instead of headless in screen
 
 # Setup disks
-disk  qcow -name "cdrive" -size 64G -if virtio
-cdrom -path "/home/sean/Windows10.iso" # Find your own iso
+disk lvmraid -name "cdrive" -mode ssd_raid10 -size 128G -ex aio=native,cache=none
+cdrom -path "$ISO_CACHE_DIR/Windows10.iso" # Find your own iso
 # Tack on a driver cd with virtio drivers because Windows will not know what to do with them
-cdrom -path "/home/sean/virtio.iso" # Available here: https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.185-2/virtio-win.iso
+cdrom -path "$ISO_CACHE_DIR/virtio.iso" # Available here: https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.185-2/virtio-win.iso
 
 # Create the network adapter
-network virtio_user
+network vlanbridge -vlan 41 # Work
 
 command_handler "$@"
